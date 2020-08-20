dbb6fbf141d0e16479c8c123ade478d79e1343b6
amagrach <Ainho1@MacBook-Pro.local>
2020-08-20 20:43:36 +0200

    pequeños cambios a los analisis

--- scripts/plot_abundancias_polinizadores.R
+++ scripts/plot_abundancias_polinizadores.R
@@ -1,9 +1,28 @@
 library(dplyr)
+library(plyr)
 library(ggplot2)
 
 seg_clean<- read.csv("./data/seg_clean.csv")
 seg_subset<- subset(seg_clean, Polinizador %in% c("Bombus hortorum", "Bombus lapidarius", "Bombus pratorum", "Bombus pascuorum", "Bombus terrestris", "Sphaerophoria scripta"))
+# 
+# # #queda feísimo pero creo que va por aquí
+#  p<- seg_subset %>% group_by(Fecha, Polinizador) %>% summarise(n=n_distinct(Codigo_vuelo)) %>% ggplot(., aes(x=Fecha, y=n)) + geom_bar(stat = 'identity') + geom_smooth() + facet_wrap(~Polinizador, scales = "free_y") + scale_y_continuous(expand = c(0,0))
+#  print(p)
 
-#queda feísimo pero creo que va por aquí
-p<- seg_subset %>% group_by(Fecha, Polinizador) %>% summarise(n=n_distinct(Codigo_vuelo)) %>% ggplot(., aes(x=Fecha, y=n)) + geom_bar(stat = 'identity') + geom_smooth() + facet_wrap(~Polinizador, scales = "free_y") + scale_y_continuous(expand = c(0,0))
-print(p)
+
+seg_subset2 <- ddply(seg_subset, c("Fecha", "Polinizador"), summarise,
+            n    = n_distinct(Codigo_vuelo))
+
+
+#te he cambiado aqui un poco la grafica para mostrar todas las especies juntas.
+
+seg_subset2$Fecha <- as.Date(seg_subset2$Fecha)
+
+head(seg_subset2)
+
+
+pl<-ggplot(seg_subset2, aes(x=Fecha, y=n, fill=Polinizador)) +
+  geom_bar(stat = "identity",  position="dodge")
+
+
+pl+scale_color_brewer(palette="Dark2")
--- scripts/posibles_analisis_a_usar.R
+++ scripts/posibles_analisis_a_usar.R
@@ -5,9 +5,37 @@ library(dplyr)
 seg<- read.csv("./data/seg_clean.csv")
 seg_subset<- subset(seg, Polinizador %in% c("Bombus hortorum", "Bombus lapidarius", "Bombus pratorum", "Bombus pascuorum", "Bombus terrestris", "Sphaerophoria scripta"))
 
-#Calcular la media y la mediana de plantas visitadas en cada seguimiento
-seg %>% group_by(Codigo_vuelo) %>% summarise(n=n()) %>% summarise(mean(n))
-seg %>% group_by(Codigo_vuelo) %>% summarise(n=n()) %>% summarise(median(n))
+# #Calcular la media y la mediana de plantas visitadas en cada seguimiento
+# seg %>% group_by(Codigo_vuelo) %>% summarise(n=n()) %>% summarise(mean(n))
+# seg %>% group_by(Codigo_vuelo) %>% summarise(n=n()) %>% summarise(median(n))
+
+
+#calcular numero de especies de plantas diferentes visitadas en cada vuelo
+seg_table <- ddply(seg_subset, c("Periodo_fecha", "Periodo_hora","Bosque","Codigo_vuelo", "Polinizador"),
+                   summarise,
+                     n_plant_sps = n_distinct(Planta))
+
+head(seg_table)
+
+#aqui estaria bien meter como covariable la diversidad total de plantas presentes en cada periodo,
+#que lo puedes sacar de los cuadrados.
+library(lme4)
+
+m1<-lmer(n_plant_sps ~ Periodo_fecha + (1|Polinizador),  data=seg_table)
+summary(m1)
+
+
+
+#calcular media y desviacion de este numero de especies de plantas visitadas por especie de polinizador y periodo
+
+seg_table2 <- ddply(seg_table, c("Periodo_fecha", "Bosque", "Polinizador"), 
+                    summarise,
+                   mean_plant_sps = mean(n_plant_sps),
+                   stdev= sd(n_plant_sps))
+
+head(seg_table2)
+
+#esto si quieres lo hablamos un dia.
 
 
 #Calcular la secuencia de plantas seguida por el polinizador
