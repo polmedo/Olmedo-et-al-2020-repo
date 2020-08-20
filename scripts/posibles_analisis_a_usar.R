566267db60a62b84562de83e8a8f30478f8c47f9
polmedo <pablolmedo94@gmail.com>
2020-08-20 02:39:11 +0100

    t-test

--- scripts/posibles_analisis_a_usar.R
+++ scripts/posibles_analisis_a_usar.R
@@ -2,8 +2,8 @@ library(tibble)
 library(data.table)
 library(dplyr)
 
-seg_clean<- read.csv("./data/seg_clean.csv")
-seg_subset<- subset(seg_clean, Polinizador %in% c("Bombus hortorum", "Bombus lapidarius", "Bombus pratorum", "Bombus pascuorum", "Bombus terrestris", "Sphaerophoria scripta"))
+seg<- read.csv("./data/seg_clean.csv")
+seg_subset<- subset(seg, Polinizador %in% c("Bombus hortorum", "Bombus lapidarius", "Bombus pratorum", "Bombus pascuorum", "Bombus terrestris", "Sphaerophoria scripta"))
 
 #Calcular la media y la mediana de plantas visitadas en cada seguimiento
 seg %>% group_by(Codigo_vuelo) %>% summarise(n=n()) %>% summarise(mean(n))
@@ -15,3 +15,8 @@ seg %>% group_by(Codigo_vuelo) %>% summarise(n=n()) %>% summarise(median(n))
 Codigo_planta<- as.numeric(seg_subset$Planta)
 seg_subset<- add_column(seg_subset, Codigo_planta, .after = 12)
 seg2<- transform(seg_subset, revisita= ave(Codigo_planta, rleid(Codigo_vuelo, Codigo_planta), FUN = seq_along))
+#UNA ALTERNATIVA QUE PARECE TENER MÁS SENTIDO Y ES MÁS SENCILLA A LO ANTERIOR
+seg3<- seg_subset %>% mutate(revisita_binario= ifelse(Planta == lag(Planta), 1, 0))
+
+#algo me falla en el t-test, me da este mensaje de error: Error: Can't combine `Fecha` <factor<fe2dc>> and `Periodo_fecha` <integer>.
+p<- seg3 %>% group_by(Polinizador) %>% t.test(revisita_binario~Periodo_fecha)
