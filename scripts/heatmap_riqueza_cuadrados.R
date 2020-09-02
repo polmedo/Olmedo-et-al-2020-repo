e055f67cdaeee42f243ad9a033598ac204bff21c
polmedo <pablolmedo94@gmail.com>
2020-09-02 02:30:36 +0100

    haciendo más bonitas las gráficas

--- scripts/heatmap_riqueza_cuadrados.R
+++ scripts/heatmap_riqueza_cuadrados.R
@@ -10,5 +10,9 @@ str(sp.rich2)
 head(sp.rich2)
 
 library(ggplot2)
+library(viridis)
 p1<-ggplot(sp.rich2, aes(x = x, y = y, fill = count)) + geom_tile()
-p1+facet_wrap(~Bosque)
+p1+scale_fill_viridis() + facet_wrap(~Bosque) + labs(fill="Plant richness") + 
+  theme(legend.title.align = 0.1, legend.title = element_text(face = "bold", size = 9), 
+        strip.background = element_blank(), strip.text = element_text(face = "bold")) + 
+  scale_x_continuous(expand = c(0,0)) + scale_y_continuous(expand = c(0,0))
--- scripts/heatmap_variacion_recursos_y_visitas.R
+++ scripts/heatmap_variacion_recursos_y_visitas.R
@@ -10,8 +10,15 @@ cuad_recursos_wide<- cuad_recursos %>%
   pivot_wider(names_from = c(periodo), values_from = abundancia, names_prefix = "periodo")
 cuad_recursos_wide$dif_periodos<- (cuad_recursos_wide$periodo2 - cuad_recursos_wide$periodo1)
 
-p1<- ggplot(cuad_recursos_wide, aes(x=x, y=y, fill=dif_periodos)) + geom_tile()
-p1 + facet_wrap(~Bosque)
+p1<- ggplot(cuad_recursos_wide, aes(x=x, y=y, fill=dif_periodos))
+p1 + ggtitle("Flower abundance variation") + 
+  geom_tile() + scale_fill_gradient2(trans="pseudo_log") + coord_equal() + 
+  facet_wrap(~Bosque, nrow = 1) + 
+  theme(plot.title = element_text(face = "bold", size = 18, hjust = 0.5), 
+        axis.title = element_text(size = 13), legend.title.align = 0.1, 
+        legend.title = element_blank(), strip.background = element_blank(), 
+        strip.text = element_text(face = "bold", size=15)) + 
+  scale_x_continuous(expand = c(0,0)) + scale_y_continuous(expand = c(0,0))
 
 #heatmap diferencia de visitas por cuadradito
 seg<- read.csv("./data/seg_clean.csv")
@@ -22,5 +29,12 @@ seg_wide<- seg_count_visitas %>%
 seg_wide$periodo1[is.na(seg_wide$periodo1)]<- 0
 seg_wide$periodo2[is.na(seg_wide$periodo2)]<- 0
 seg_wide$dif_visitas<- (seg_wide$periodo2 - seg_wide$periodo1)
-p2<- ggplot(seg_wide, aes(x=X, y=Y, fill=dif_visitas)) + geom_tile()
-p2 + facet_wrap(~Bosque)
+p2<- ggplot(seg_wide, aes(x=X, y=Y, fill=dif_visitas))
+p2 + ggtitle("Visitation rate variation") + 
+  geom_tile() + scale_fill_gradient2(trans="pseudo_log") + coord_equal() + 
+  facet_wrap(~Bosque, nrow = 1) + 
+  theme(plot.title = element_text(face = "bold", size = 18, hjust = 0.5), 
+        axis.title = element_text(size = 13), legend.title.align = 0.1, 
+        legend.title = element_blank(), strip.background = element_blank(), 
+        strip.text = element_text(face = "bold", size=15)) + 
+  scale_x_continuous(expand = c(0,0)) + scale_y_continuous(expand = c(0,0))
