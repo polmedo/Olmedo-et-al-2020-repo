696d2b51a7f6eaaf322f1d153359f4761453841a
polmedo <pablolmedo94@gmail.com>
2020-09-07 23:09:33 +0100

    Heatmap sup. material

--- scripts/heatmap_riqueza_cuadrados.R
+++ scripts/heatmap_riqueza_cuadrados.R
@@ -11,8 +11,8 @@ head(sp.rich2)
 
 library(ggplot2)
 library(viridis)
-p1<-ggplot(sp.rich2, aes(x = x, y = y, fill = count)) + geom_tile()
-p1+scale_fill_viridis() + facet_wrap(~Bosque) + labs(fill="Plant richness") + 
+p1<-ggplot(sp.rich2, aes(x = x, y = y, fill = count)) + geom_tile() + coord_equal()
+p1+scale_fill_viridis(begin=0.1) + facet_wrap(~Bosque) + labs(fill="Plant richness") + 
   theme(legend.title.align = 0.1, legend.title = element_text(face = "bold", size = 9), 
         strip.background = element_blank(), strip.text = element_text(face = "bold")) + 
   scale_x_continuous(expand = c(0,0)) + scale_y_continuous(expand = c(0,0))
